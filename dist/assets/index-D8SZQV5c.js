bfb85e3c36cf905bedce35e9e84a859ea6804ca5
P-PPT <76959908+PT-VU@users.noreply.github.com>
2026-01-07 18:40:06 +0100

    debug2

--- dist/assets/index-D8SZQV5c.js
+++
@@ -1,1 +0,0 @@
-import{K as a}from"./app-DbnccKte.js";import"./index-CkK5ANc4.js";var t=a;export{t as default};
---
+++ dist/assets/index-Di3GE8fF.js
@@ -0,0 +1,1 @@
+import{g as a,a as e,c as t}from"./app-BfETAE6d.js";import"./index-DDmmewoZ.js";const r={renderer:t,...e,...a};var i=r;export{i as default};
